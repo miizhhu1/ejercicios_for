b189699ad92605c4a5f61c78852725b871448331
Miizhhu1 <miizhhu@gmail.com>
2026-07-20 23:12:11 -0500

    crear tabla de multipicar personalizada

--- tablas/multiplicar.js
+++ tablas/multiplicar.js
@@ -1,10 +1,11 @@
 function generarTablas() {
     let divContenedor = document.getElementById("divContenedor");
     let contenido = "";
+    let numero= parseInt(document.getElementById("txtNumero").value);
 
     for(let i=1; i<=12; i++ )
     {
-       contenido = contenido + "<div class='fila'>3 × " + i + " = " + (5 * i) + "</div>";
+       contenido = contenido + "<div class='fila'>"+ numero + "× " + i + " = " + (numero * i) + "</div>";
         
     }
     divContenedor.innerHTML = contenido;
